262e83c1deb2f92f9a5240c8750006ffb91d13e0
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2024-02-24 18:25:44 +0800

    Created the first component which is sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-        <div className='AppGlass'>
-          
-        </div>
+      <div className='AppGlass'>
+        <Sidebar />
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
