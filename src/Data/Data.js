33e3bcf4ef4b4ed578b96d01233626873a24ac44
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2024-02-24 19:29:14 +0800

    Created the Sidebar menu

---
+++ src/Data/Data.js
@@ -0,0 +1,30 @@
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart
+} from "@iconscout/react-unicons";
+
+export const SidebarData = [
+    {
+        icon: UilEstate,
+        title: "Dashboard"
+    },
+    {
+        icon: UilClipboardAlt,
+        title: "Orders"
+    },
+    {
+        icon: UilUsersAlt,
+        title: "Customers"
+    },
+    {
+        icon: UilPackage,
+        title: "Products"
+    },
+    {
+        icon: UilChart,
+        title: "Analytics"
+    }
+];
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
+import Logo from '../../imgs/logo.png'
+import { SidebarData } from '../../Data/Data'
+import { UilSignOutAlt } from '@iconscout/react-unicons'
 
 const Sidebar = () => {
+
+    const [selected, setSelected] = useState(0)
+
     return (
-        <div>Sidebar</div>
+        <div className="Sidebar">
+            <div className="logo">
+                <img src={Logo} alt="" />
+                <span>
+                    Sh<span>o</span>ps
+                </span>
+            </div>
+
+            {/* menu */}
+            <div className="menu">
+                {SidebarData.map((item, index) => {
+                    return (
+                        <div className={selected === index ? 'menuItem active' : 'menuItem'}
+                            key={index}
+                            onClick={() => setSelected(index)}
+                        >
+
+                            <item.icon />
+                            <span>
+                                {item.title}
+                            </span>
+                        </div>
+                    )
+                })}
+
+                <div className="menuItem">
+                    <UilSignOutAlt />
+                    <span>
+                        Logout
+                    </span>
+                </div>
+            </div>
+        </div >
     )
 }
 
