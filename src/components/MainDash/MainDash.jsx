ac78468bdd914dbc1ddc4594e7245c5779f7d60c
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2024-02-26 11:42:41 +0800

    Added the Table at the bottom of MainDash component

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './MainDash.css'
 import Cards from '../Cards/Cards'
+import Table from '../Table/Table'
 
 const MainDash = () => {
     return (
         <div className="MainDash">
             <h1>Dashboard</h1>
             <Cards />
+            <Table />
         </div>
     )
 }
